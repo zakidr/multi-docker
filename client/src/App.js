928755d50b7716f9fddc4b107ca5b046f26535ef
zakidr <z.idrissi@hcp.ma>
2024-02-15 11:05:17 +0000

    final deployement3

--- client/src/App.js
+++ client/src/App.js
@@ -8,27 +8,26 @@ import Fib from './Fib';
 function App() {
   return (
     <Router>
-     <div className="App">
-       <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         <h1 className="App-title">Fib Calculator</h1>
-         <Link to="/">Home</Link>
-	 <Link to="/otherpage">Other Page</Link>
-         <div>
-           <Route exact path="/" component={Fib} />
-	   <Route path="/otherpage" component={OtherPage} />
-         </div>
-         <a
-           className="App-link"
-           href="https://reactjs.org"
-           target="_blank"
-           rel="noopener noreferrer"
-         >
-           Learn React
-         </a>
-       </header>
-     </div>
-   </Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Fib Calculator
+          </a>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
